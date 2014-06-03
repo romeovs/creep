a57673425756e7963e4f1e3133ddfb4f6eda37cd
Romeo Van Snick <romeovs@gmail.com>
2014-06-03 22:23:42 +0200

    add all arrows to hhl.sh

--- hhl.sh
+++ hhl.sh
@@ -21,7 +21,9 @@ comment="s/--.*$/$BLACK&$RESET/g;"
 compose="s/ \. / ${RED}·$RESET /g;"
 compose_operator="s/\(\.\)/(${RED}·$RESET)/g;"
 double_colon="s/::/$RED&$RESET/g;"
-double_right_arrow="s/=>/$RED=⇒$RESET/g;"
+double_right_arrow="s/=>/=⇒/g;"
+double_right_arrow_tail="s/([^>])>=>/\1=⇒/g;"
+double_right_arrow_double_tail="s/>>=>/=⇒/g;"
 inlined="s/\`[^\`]*\`/$RED&$RESET/g;"
 interactive="s/(^<[^>]*>|<interactive>)/$YELLOW&$RESET/g;"
 keyword="s/where/$RED&$RESET/g;"
@@ -31,20 +33,28 @@ lambda3="s/λ/${RED}&$RESET/g;"
 keyword_line="s/^[\ ]*(instance|class|where|data|type|newtype)/$RED&$RESET/g;"
 load_failed="s/^Failed, modules loaded:/$RED&$RESET/;"
 load_done="s/done./$PURPLE&$RESET/g;"
-monad_bind="s/>>=/${RED}=$RESET/g;"
-monad_bind_rev="s/=<</${RED}=$RESET/g;"
+monad_bind="s/>>=/=/g;"
+monad_bind_rev="s/=<</=/g;"
 no_instance="s/^\s*No instance/$RED&$RESET/g;"
 operator="s/(\(| )([!$%&*+.\/<=>?@\\\^|-~:]*)(\)| )/\1$RED\2$RESET\3/g;"
-right_arrow="s/->/$RED-${RED}→$RESET/g;"
+arrow="s/(\(| )([=⇒-]*)(\)| )/\1$RED\2$RESET\3/g;"
+right_arrow2="s/ ->/ ${RED}-→$RESET/g;"
+right_arrow="s/->/-${RED}→$RESET/g;"
+right_arrow_tail="s/([^>])>-/\1${RED}⤚-${RESET}/g;"
+right_arrow_double_tail="s/>>-/${RED}⤜-${RESET}/g;"
+left_arrow="s/<-/${RED}←${RESET}-/g;"
+left_arrow2="s/<- /${RED}←-${RESET} /g;"
+left_arrow_tail="s/-<([^<])/${RED}-⤙${RESET}\1/g;"
+left_arrow_double_tail="s/-<</${RED}-⤛<${RESET}/g;"
 string="s/\"[^\"]*\"/$GREEN&$RESET/g;"
-
-# !#$%&*+./<=>?@\^|-~:
 
 sed -E "$character\
         $comment\
         $compose\
         $compose_operator\
         $double_colon\
+        $double_right_arrow_tail\
+        $double_right_arrow_double_tail\
         $double_right_arrow\
         $keyword\
         $keyword_line\
@@ -53,11 +63,19 @@ sed -E "$character\
         $lambda\
         $lambda2\
         $lambda3\
+        $left_arrow\
+        $left_arrow2\
+        $left_arrow_tail\
+        $left_arrow_double_tail\
         $load_failed\
         $load_done\
         $monad_bind\
         $monad_bind_rev\
         $no_instance\
-        $operator\
+        $right_arrow2\
         $right_arrow\
-        $string"
+        $right_arrow_tail\
+        $right_arrow_double_tail\
+        $string\
+        $arrow\
+        $operator"
